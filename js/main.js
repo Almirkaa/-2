3067d6a25e577108655e7496251187803c8d7cc1
Almirkaa <almigarifullina@gmail.com>
2024-09-05 13:19:34 +0400

    js практика2

---
+++ js/main.js
@@ -0,0 +1,39 @@
+'user strict';
+let admin;
+let name = 'Джон';
+admin=name;
+alert(admin);
+
+
+let nameCity = prompt('Напишите название города');
+let yearCity = prompt('Напишите год образования');
+let peopleCity = prompt('Напишите количество населения');
+
+let ageCity = 2024 - yearCity;
+alert(`Городу ${nameCity} исполнилось ${ageCity} лет с момента его образования. Население - ${peopleCity} человек`);
+
+
+
+
+
+let r = prompt('Радиус круга равен');
+let S = 3.14 * r**2;
+alert(`Площадь круга с радиусом ${r} равна ${S}`);
+
+
+let a = Number(prompt('Ввведите первое число'));
+let b = Number (prompt('Ввведите второе число'));    
+alert(`Сумма чисел ${a+b}`);
+alert(`Разность чисел ${a-b}`);
+alert(`Произведение чисел ${a*b}`);
+alert(`Частное чисел ${a/b}`);
+
+
+
+
+
+
+
+
+
+
